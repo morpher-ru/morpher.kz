84fddc11c2c9d5ad72ce1024ae9a114351e4321f
Sergey Slepov <slepov@gmail.com>
2019-08-18 19:14:56 +0300

    Basic website that redirects to http://morpher.ru/DemoKZ.aspx

---
+++ morpher.kz/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace morpher.kz
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                HttpResponse response = context.Response;
+                response.StatusCode = 302;
+                response.Headers.Add("location", "http://morpher.ru/DemoKZ.aspx");
+                await response.WriteAsync("Moved temporarily.");
+            });
+        }
+    }
+}
